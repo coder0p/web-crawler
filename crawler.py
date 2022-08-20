c9d66550dae20bf8d0c202bec711a61a37c85b06
Aldrin Thomas <aldrinthomas300@gmail.com>
2022-08-20 19:47:31 +0530

    Updation crawler.py -> Create two additional functions named create_tables() and add_database to create table query from init.sql file, adds scrapped data of artist's names, songs name and lyrcis ->added subcommands in parser to initialize , crawl and add directory of artists

--- crawler.py
+++ crawler.py
@@ -6,14 +6,25 @@ import os
 import requests
 from bs4 import BeautifulSoup
 
+
+import db
+
+
 logger = None
+dbname = "lyrics"
 
 def parse_args():
     parser = argparse.ArgumentParser(description = "Web crawler")
     parser.add_argument("-d", "--debug", help = "Enable debug logging", action="store_true")
+    parser.add_argument("--db", help="Name of database to use")
+    subcommands = parser.add_subparsers(help="Commands", dest="command", required=True)
+    subcommands.add_parser("addir", help="Adding directory named artists to store lyrics")
+    subcommands.add_parser("initdb", help="Initialise the database")
+    subcommands.add_parser("crawl", help="Perform a crawl")
+    subcommands.add_parser("web", help="Start web server")
     return parser.parse_args()
 
-def configure_logging(level=logging.INFO):
+def configure_logging(level=logging.DEBUG):
     global logger
     logger = logging.getLogger("crawler")
     logger.setLevel(level)
@@ -24,7 +35,7 @@ def configure_logging(level=logging.INFO):
     logger.addHandler(screen_handler)
 
 
-def get_artists(base):
+def get_artists_list(base):
     """Returns a dictionary of artists with argument of popular artist link"""
     
     artists = {}
@@ -38,6 +49,7 @@ def get_artists(base):
         logger.debug('artist list parsed successfully')
         for heading in headings[:5]:
             artists[heading.text] = heading.a['href']
+           
     else:
         logger.debug("artist list could not parsed successfully")
 
@@ -53,18 +65,19 @@ def get_song_list(base):
     logger.debug("status: %s",res.status_code)
     soup = BeautifulSoup(res.content, "lxml")
     track = soup.find("table", attrs={"class": "tracklist"})
-    links = track.find_all('a')
-    if links:
+    songs = track.find_all('a')
+    if songs:
         logger.debug("song list parsed successfully")
-        for link in links[:5]:
-            songs_link[link.text] = link["href"]
+        for song in songs[:5]:
+            songs_link[song.text] = song["href"]
+            
     else:
         logger.debug("song list could not parsed successfully")
 
     return songs_link
 
 
-def get_lyrics(base):
+def get_song_lyrics(base):
     """Return lyrics string which is corresponding to selected song"""
 
     logger.debug(f"requesting {base} ...")
@@ -79,11 +92,10 @@ def get_lyrics(base):
 
     return lyrics.text
 
-
 def crawl(download_dir):
     """Funtion for downloading lyrics and storing in artist's directory"""
 
-    for artist_name ,artist_link in get_artists("https://www.songlyrics.com/top-artists-lyrics.html").items():
+    for artist_name ,artist_link in get_artists_list("https://www.songlyrics.com/top-artists-lyrics.html").items():
         logger.debug("Artist:  %s", artist_name)
         artist_dirt = os.path.join(download_dir, artist_name)
         artist_dirt = artist_dirt.lower()
@@ -93,21 +105,63 @@ def crawl(download_dir):
             song_name = song.replace("/","-")
             song_name = song_name.lower()
             with open(f"{artist_dirt}/{song_name}-lyrics.txt",'w') as file:
-                n_bytes = file.write(get_lyrics(song_link))
+                n_bytes = file.write(get_song_lyrics(song_link))
                 if n_bytes < 5:
                     logger.debug("File size is too short!")
                 else:
                     logger.debug("File downloaded successfully")
 
+
+def create_tables():
+    """This function creates table with given sql file into the database mentioned """
+
+    conn = db.get_connection(dbname)
+    with conn.cursor() as cursor:
+        with open("init.sql") as f:
+            sql = f.read()
+            cursor.execute(sql)
+    conn.commit()
     
+
+def add_database():
+    """ This function adds the artist name and song's lyrics into database """
+
+    conn = db.get_connection(dbname)
+    for artist_name, artist_link in get_artists_list('http://www.songlyrics.com/top-artists-lyrics.html').items():
+        last_id = db.add_artist(conn,artist_name)
+        for song, song_link in get_song_list(artist_link).items():
+            lyrics = get_song_lyrics(song_link)
+        db.add_song(conn,song, last_id, lyrics)
+    conn.close()
+
+
 def main():
     args = parse_args()
+
     if args.debug:
         configure_logging(logging.DEBUG)
     else:
         configure_logging(logging.INFO)
+
     
-    crawl('artists')
+    if args.command == "crawl":
+        logger.info("Scraping song-lyrics.com ....")
+        add_database()
+        logger.info("Scraping completed!")
+    elif args.command == "initdb":
+        logger.info("Initializing database....")
+        create_tables()
+        logger.info("%s database initialized!!", dbname)
+    elif args.command == "addir":
+        logger.info("Creating directory...")
+        crawl('artists')
+        logger.info("Directory created successfully!")
+    else:
+        logger.warning("%s not implemented!!!", args.command)
+    
+
+ 
 
 if __name__ == "__main__":
     main()
+    
